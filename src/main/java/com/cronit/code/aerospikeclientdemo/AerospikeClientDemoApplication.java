f7b62a98cd4ed2e43e1fc752bfcc52413d47b313
ronit.chattopadhyay <ronit.chattopadhyay@flipkart.com>
2023-08-16 10:14:42 +0530

    Init Commit

---
+++ src/main/java/com/cronit/code/aerospikeclientdemo/AerospikeClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cronit.code.aerospikeclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerospikeClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerospikeClientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cronit/code/aerospikeclientdemo/AerospikeClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cronit.code.aerospikeclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerospikeClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
